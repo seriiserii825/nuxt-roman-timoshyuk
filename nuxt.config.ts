f95fcff1b4ab0e28f8a854ad1a4921253c46e9ff
serii <seriiburduja@gmail.com>
2025-12-13 11:06:45 +0200

    feat: aded all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primeuix/themes/aura";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devServer: {
+    port: Number(process.env.NUXT_PORT) || 3000,
+  },
+
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/scss/my.scss"],
+  ssr: false,
+
+  app: {
+    head: {
+      link: [{ rel: "stylesheet", href: "/css/fonts.css" }],
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+
+  typescript: {
+    typeCheck: true,
+  },
+
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+
+  experimental: {
+    typedPages: true,
+  },
+
+  modules: ["@pinia/nuxt", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
+
