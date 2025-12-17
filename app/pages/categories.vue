24b0a01f0eb1f277e6df45f096338681a212f41a
serii <seriiburduja@gmail.com>
2025-12-17 22:09:21 +0200

    feat: tailwind prettier

--- app/pages/categories.vue
+++ app/pages/categories.vue
@@ -5,8 +5,21 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="categories-page">
-    <h1 class="text-3xl font-bold mb-6">Categories Page</h1>
-    <p class="text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus voluptate saepe optio perferendis temporibus ipsa placeat nesciunt. Quas corrupti architecto iste beatae labore, dolorum molestiae, nesciunt, impedit vero tempora minus!</p>
+  <div class="mt-10 rounded-md bg-slate-800 p-4">
+    <div class="mt-2 flex flex-wrap items-center gap-2">
+      <div
+        class="group relative flex cursor-pointer items-center gap-2 rounded-lg bg-blue-600 px-4 py-2"
+      >
+        <span>Salary</span>
+        <div
+          class="pointer-events-none absolute inset-0 flex items-center justify-between rounded-lg bg-black/90 px-3 opacity-0 transition-opacity duration-200 group-hover:pointer-events-auto group-hover:opacity-100"
+        >
+          <i
+            class="pi pi-pen-to-square transition-colors hover:text-green-600"
+          />
+          <i class="pi pi-trash transition-colors hover:text-red-600" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
