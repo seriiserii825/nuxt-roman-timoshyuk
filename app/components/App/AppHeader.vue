b2d11680bd9329c93364edddcda551347b4b77d1
serii <seriiburduja@gmail.com>
2025-12-18 20:40:10 +0200

    feat: header buttons

--- app/components/App/AppHeader.vue
+++ app/components/App/AppHeader.vue
@@ -14,27 +14,26 @@ function logout() {
 </script>
 
 <template>
-  <div class="flex items-center justify-between gap-4 px-8 py-4 bg-gray-800 text-white">
-    <NuxtLink to="/"><i class="pi pi-bitcoin" style="font-size: 1.8rem"></i></NuxtLink>
-    <nav v-if="is_auth" class="flex items-center gap-6 ml-auto text-white/50">
+  <div
+    class="flex items-center justify-between gap-4 bg-gray-800 px-8 py-4 text-white"
+  >
+    <NuxtLink to="/"
+      ><i class="pi pi-bitcoin" style="font-size: 1.8rem"></i
+    ></NuxtLink>
+    <nav v-if="is_auth" class="ml-auto flex items-center gap-6 text-white/50">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/categories">Categories</NuxtLink>
       <NuxtLink to="/transactions">Transactions</NuxtLink>
     </nav>
     <div v-if="is_auth">
-      <Button
-        @click="logout"
-        class="btn btn-danger"
-        label="Logout"
-        icon="pi pi-sign-out"
-        severity="help" />
+      <button @click="logout" class="btn btn-danger">Logout</button>
     </div>
     <div v-else>
       <NuxtLink to="/auth?login=true">
-        <Button class="btn" label="Login" icon="pi pi-sign-in" severity="help" />
+        <button class="btn" severity="help">Login</button>
       </NuxtLink>
       <NuxtLink to="/auth?login=false">
-        <Button class="btn btn-info ml-2" label="Register" icon="pi pi-user-plus" severity="info" />
+        <button class="btn btn-info ml-2">Register</button>
       </NuxtLink>
     </div>
   </div>
