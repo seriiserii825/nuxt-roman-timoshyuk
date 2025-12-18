8b7f5475cadd84e53bb96dd55a1b90445ffe3b3e
serii <seriiburduja@gmail.com>
2025-12-18 21:36:45 +0200

    feat: start transactions page

--- app/pages/transactions.vue
+++ app/pages/transactions.vue
@@ -5,12 +5,23 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-  <div class="">transactions</div>
-  <p class="text-lg">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus voluptate saepe optio
-    perferendis temporibus ipsa placeat nesciunt. Quas corrupti architecto iste beatae labore,
-    dolorum molestiae, nesciunt, impedit vero tempora minus!
-  </p>
+  <div class="mt-4 grid grid-cols-3 items-start gap-4">
+    <div class="col-span-2 grid">Add transaction form</div>
+    <div class="shape">
+      <div class="grid grid-cols-2 gap-3">
+        <div>
+          <p class="text-md text-center font-bold uppercase">Total Income:</p>
+          <p class="mt-2 rounded-sm bg-green-600 p-1 text-center font-bold">
+            1000$
+          </p>
+        </div>
+        <div>
+          <p class="text-md text-center font-bold uppercase">Total Expense:</p>
+          <p class="mt-2 rounded-sm bg-red-600 p-1 text-center font-bold">
+            1000$
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
